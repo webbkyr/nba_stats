e66af27dd0c6c9c1f08398aaa48b320dc639a8fc
Kayla Webb <kaylarwebb@gmail.com>
2018-04-14 22:48:50 -0400

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  scope path: '/nba_api' do
+    scope path: '/v1' do
+      # your routes go here
+    end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end

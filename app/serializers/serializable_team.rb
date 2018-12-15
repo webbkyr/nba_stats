acc1564f597fbf49a72681c81bc67d77d9d80d9b
Kayla Webb <kaylarwebb@gmail.com>
2018-12-15 08:10:53 -0500

    chore: address security vulnerabilities

--- app/serializers/serializable_team.rb
+++ app/serializers/serializable_team.rb
@@ -2,10 +2,11 @@
 # We use jsonapi-rb, which is similar to active_model_serializers.
 class SerializableTeam < JSONAPI::Serializable::Resource
   type :teams
-  
+
   attribute :name
   attribute :city
   attribute :state
   attribute :team_id
   attribute :conference
+  
 end
